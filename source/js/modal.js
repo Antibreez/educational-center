01879005c50a554f871c38c99346069936569845
Anton <kiyananton@gmail.com>
2022-09-27 15:44:47 +0300

    edtis

--- source/js/modal.js
+++ source/js/modal.js
@@ -2,7 +2,7 @@ const $triggers = $("[data-trigger]");
 const $overlay = $(".modal__overlay");
 const $closeBtn = $(".modal__close-btn");
 
-$overlay.on("click", (e) => {
+$(document).on("click", ".modal__overlay", function (e) {
   const $target = $(e.target);
 
   if ($target.parents(".modal__body").length > 0) return;
@@ -11,16 +11,39 @@ $overlay.on("click", (e) => {
   $("body").removeClass("js-no-scroll");
 });
 
-$closeBtn.on("click", (e) => {
+// $overlay.on("click", (e) => {
+//   const $target = $(e.target);
+
+//   if ($target.parents(".modal__body").length > 0) return;
+
+//   $target.parents(".modal").removeClass("js-show");
+//   $("body").removeClass("js-no-scroll");
+// });
+
+$(document).on("click", ".modal__close-btn", function (e) {
   const $target = $(e.currentTarget);
 
   $target.parents(".modal").removeClass("js-show");
   $("body").removeClass("js-no-scroll");
 });
 
-$triggers.on("click", (e) => {
+// $closeBtn.on("click", (e) => {
+//   const $target = $(e.currentTarget);
+
+//   $target.parents(".modal").removeClass("js-show");
+//   $("body").removeClass("js-no-scroll");
+// });
+
+$(document).on("click", "[data-trigger]", function (e) {
   const name = $(e.currentTarget).attr("data-trigger");
 
   $(`.modal[data-modal='${name}']`).addClass("js-show");
   $("body").addClass("js-no-scroll");
 });
+
+// $triggers.on("click", (e) => {
+//   const name = $(e.currentTarget).attr("data-trigger");
+
+//   $(`.modal[data-modal='${name}']`).addClass("js-show");
+//   $("body").addClass("js-no-scroll");
+// });
